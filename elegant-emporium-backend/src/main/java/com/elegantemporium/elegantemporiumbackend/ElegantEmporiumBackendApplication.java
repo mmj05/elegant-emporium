19e6cf3c12c77779d0a03caa64500ad2d8be131e
muhaiminjobayer <mmj0993@gmail.com>
2023-12-05 12:44:21 -0600

    Initialized backend with spring initializer

---
+++ elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/ElegantEmporiumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elegantemporium.elegantemporiumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElegantEmporiumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElegantEmporiumBackendApplication.class, args);
+	}
+
+}
---
+++ elegant-emporium-backend/src/test/java/com/elegantemporium/elegantemporiumbackend/ElegantEmporiumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elegantemporium.elegantemporiumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElegantEmporiumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
