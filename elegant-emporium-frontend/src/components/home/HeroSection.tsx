2eaf9d6956cda4a48d546448d5986ec5387e874a
muhaiminjobayer <mmj0993@gmail.com>
2023-12-08 09:58:11 -0600

    Wrote initial code for HeroSection component

---
+++ elegant-emporium-frontend/src/components/home/HeroSection.tsx
@@ -0,0 +1,25 @@
+// src/components/home/HeroSection.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HeroSection: React.FC = () => {
+    return (
+        <div
+            style={{
+                marginTop: 55,
+                paddingTop: 55,
+            }}
+            className='container-fluid text-sm-center p-5 bg-info text-white'
+        >
+            <h1>Welcome to Our E-commerce Store!</h1>
+            <p>Discover amazing products and deals.</p>
+            <p>
+                <Link to='/shop' className='btn btn-primary btn-lg'>
+                    Shop Now
+                </Link>
+            </p>
+        </div>
+    );
+};
+
+export default HeroSection;
