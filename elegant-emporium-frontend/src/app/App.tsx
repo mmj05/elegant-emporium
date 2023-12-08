f931d4b1d5366603c944f6a71c271049133039af
muhaiminjobayer <mmj0993@gmail.com>
2023-12-08 09:57:17 -0600

    Implemented route

--- elegant-emporium-frontend/src/app/App.tsx
+++ elegant-emporium-frontend/src/app/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-    return <div className='App'>Learn React</div>;
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from '../components/pages/Home';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Container>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Container>
+    </Router>
+  );
+};
 
 export default App;
