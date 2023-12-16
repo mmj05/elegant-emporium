4ed17ec341bb5c374f7aa99f1c05b61dddf4e403
muhaiminjobayer <mmj0993@gmail.com>
2023-12-16 11:32:59 -0600

    Conneted global state to the app

--- elegant-emporium-frontend/src/app/App.tsx
+++ elegant-emporium-frontend/src/app/App.tsx
@@ -4,18 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from '../components/pages/Home';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Container>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-        <Footer />
-      </Container>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Container>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Home} />
+          </Switch>
+          <Footer />
+        </Container>
+      </Router>
+    </Provider>
   );
 };
 
