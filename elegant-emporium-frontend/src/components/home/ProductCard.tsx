ea4753776507318a00ade6d6e5618054f35d4f38
muhaiminjobayer <mmj0993@gmail.com>
2023-12-16 11:33:32 -0600

    Updated Product interface

--- elegant-emporium-frontend/src/components/home/ProductCard.tsx
+++ elegant-emporium-frontend/src/components/home/ProductCard.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 
 interface Product {
-  id: number;
   name: string;
   price: number;
+  gender: string;
+  category: string;
+  description: string;
   image: string;
+  quantity: number;
+  _links: {
+    self: {
+      href: string;
+    };
+    product: {
+      href: string;
+    };
+  };
 }
 
 interface ProductCardProps {
@@ -19,6 +30,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
       <Card.Body>
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>${product.price}</Card.Text>
+        <Card.Text>{product.description}</Card.Text>
         <Button variant="primary">Add to Cart</Button>
       </Card.Body>
     </Card>
