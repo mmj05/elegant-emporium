255ff3502111fc3cab07bb8b16dfd51b6831c13f
muhaiminjobayer <mmj0993@gmail.com>
2023-12-16 11:32:07 -0600

    Configured cors for ProductRepository class

--- elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/dao/ProductRepository.java
+++ elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/dao/ProductRepository.java
@@ -2,6 +2,10 @@ package com.elegantemporium.elegantemporiumbackend.dao;
 
 import com.elegantemporium.elegantemporiumbackend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@RepositoryRestController
+@CrossOrigin(origins = "http://localhost:3000")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
