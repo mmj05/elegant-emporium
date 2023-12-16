85dd8df83d8009c06a8607339b61d7b93217b419
muhaiminjobayer <mmj0993@gmail.com>
2023-12-16 11:34:51 -0600

    Removed new arrivals section from the Promotional section

--- elegant-emporium-frontend/src/components/home/PromotionalSection.tsx
+++ elegant-emporium-frontend/src/components/home/PromotionalSection.tsx
@@ -5,16 +5,11 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 const PromotionalSection: React.FC = () => {
   return (
     <Container className="mt-4">
-      <Row>
-        <Col>
+      <Row className="justify-content-md-center">
+        <Col md="6">
           <h2>Special Offers</h2>
           <p>Check out our exclusive deals and save big!</p>
           <Button variant="info">View Offers</Button>
-        </Col>
-        <Col>
-          <h2>New Arrivals</h2>
-          <p>Discover the latest products in our collection.</p>
-          <Button variant="info">Explore Now</Button>
         </Col>
       </Row>
     </Container>
