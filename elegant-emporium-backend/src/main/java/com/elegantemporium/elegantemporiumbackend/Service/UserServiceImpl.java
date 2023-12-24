ddbc72957277e7f91529855d944afb09644e3157
muhaiminjobayer <mmj0993@gmail.com>
2023-12-24 12:24:22 -0600

    Created UserService implementation

---
+++ elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/Service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.elegantemporium.elegantemporiumbackend.Service;
+
+import com.elegantemporium.elegantemporiumbackend.dao.UserRepository;
+import com.elegantemporium.elegantemporiumbackend.entity.User;
+import com.elegantemporium.elegantemporiumbackend.model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public User registerUser(UserModel userModel) {
+        User user = new User();
+        user.setEmail(userModel.getEmail());
+        user.setFirstName(userModel.getFirstName());
+        user.setLastName(userModel.getLastName());
+        user.setRole("USER");
+        user.setPassword(passwordEncoder.encode(userModel.getPassword()));
+
+        userRepository.save(user);
+
+        return user;
+    }
+}
