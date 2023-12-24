d9532714a193081ec015d7663c3bc1902af60d41
muhaiminjobayer <mmj0993@gmail.com>
2023-12-24 12:23:16 -0600

    Updated User

--- elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/entity/User.java
+++ elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/entity/User.java
@@ -1,24 +1,44 @@
 package com.elegantemporium.elegantemporiumbackend.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id
-    @Column(name = "username", length = 50)
-    private String username;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column(name = "password", length = 500)
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "password", length = 60, nullable = false)
     private String password;
 
     @Column(name = "enabled")
     private boolean enabled;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String role, String password, boolean enabled) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+        this.enabled = enabled;
+    }
 }
