354107af23d2824e62025e5b44102d8efff37ca7
muhaiminjobayer <mmj0993@gmail.com>
2023-12-24 12:23:33 -0600

    Updated UserRepository

--- elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/dao/UserRepository.java
+++ elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/dao/UserRepository.java
@@ -3,8 +3,5 @@ package com.elegantemporium.elegantemporiumbackend.dao;
 import com.elegantemporium.elegantemporiumbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
 }
