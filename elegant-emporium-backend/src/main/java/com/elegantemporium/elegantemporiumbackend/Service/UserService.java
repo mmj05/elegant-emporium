4c4215382a239f540218e0dcb8569c707bc8dba4
muhaiminjobayer <mmj0993@gmail.com>
2024-01-03 14:20:50 -0600

    Created UserService

--- elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/Service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.elegantemporium.elegantemporiumbackend.Service;
-
-import com.elegantemporium.elegantemporiumbackend.entity.User;
-import com.elegantemporium.elegantemporiumbackend.model.UserModel;
-
-public interface UserService {
-    User registerUser(UserModel userModel);
-}
