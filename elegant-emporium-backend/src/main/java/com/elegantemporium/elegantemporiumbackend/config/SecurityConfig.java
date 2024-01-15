cae9b85f003776eef33b044f9f798be50398bced
muhaiminjobayer <mmj0993@gmail.com>
2024-01-15 13:29:08 -0600

    Made minor update to allow request to certain endpoints.

--- elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/config/SecurityConfig.java
+++ elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/config/SecurityConfig.java
@@ -1,15 +1,42 @@
 package com.elegantemporium.elegantemporiumbackend.config;
 
+import com.elegantemporium.elegantemporiumbackend.Service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/secure/**").authenticated()
+                        .requestMatchers("/secure/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/products", "/signup", "/login", "/logout").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsServiceImpl customUserDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(16);
+        return new BCryptPasswordEncoder();
     }
 }
