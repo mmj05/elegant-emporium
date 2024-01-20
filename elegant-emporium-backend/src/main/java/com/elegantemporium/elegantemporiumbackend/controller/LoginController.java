976cfcbcc09e2733977ee0f910e04b13bde53159
muhaiminjobayer <mmj0993@gmail.com>
2024-01-19 21:02:01 -0600

    Created LoginController.java: Added functionality to register user

--- elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/controller/LoginController.java
+++ elegant-emporium-backend/src/main/java/com/elegantemporium/elegantemporiumbackend/controller/LoginController.java
@@ -1,23 +1,32 @@
-//package com.elegantemporium.elegantemporiumbackend.controller;
-//
-//import com.elegantemporium.elegantemporiumbackend.model.UserModel;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class LoginController {
-//
-//    private final AuthenticationManager authenticationManager;
-//
-//    public LoginController(AuthenticationManager authenticationManager) {
-//        this.authenticationManager = authenticationManager;
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<Void> login(@RequestBody UserModel loginRequest) {
-//        return ResponseEntity.ok().build();
-//    }
-//}
+package com.elegantemporium.elegantemporiumbackend.controller;
+
+import com.elegantemporium.elegantemporiumbackend.dao.UserRepository;
+import com.elegantemporium.elegantemporiumbackend.entity.User;
+import com.elegantemporium.elegantemporiumbackend.model.UserModel;
+import com.elegantemporium.elegantemporiumbackend.util.UserModelToUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserModelToUserMapper userModelToUserMapper;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody UserModel userModel) {
+        if (userRepository.existsByEmail(userModel.getEmail())) {
+            return ResponseEntity.badRequest().body("Error: Email is already taken!");
+        }
+
+        User user = userModelToUserMapper.mapUserModelToUser(userModel);
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully!");
+    }
+}
